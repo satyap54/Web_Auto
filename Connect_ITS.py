e2dd63e62703ab1e2593c1c00d76e9ff16d75a11
Satyabrat Panda <46991649+encryptedbrat2000@users.noreply.github.com>
2019-08-21 00:52:46 +0530

    Update Connect_ITS.py

--- Connect_ITS.py
+++ Connect_ITS.py
@@ -6,9 +6,9 @@ browser=webdriver.Firefox()
 browser.get('http://gstatic.com/generate_204')
 #button_to_click=browser.find_element_by_css_selector('div.fer:nth-child(7) > input:nth-child(1)')
 user_name=browser.find_element_by_css_selector('#un')
-user_name.send_keys('B118054')
+user_name.send_keys('ID') #put your ID here.
 
 pswd=browser.find_element_by_css_selector('#pd')
-pswd.send_keys(os.environ["INFORMAL_MAIL_PASS"])
+pswd.send_keys(os.environ["ID_PASS"]) #initialize your ID_PASS var. in .bashrc to your password to login to ITS
 pswd.submit()
 #ele=browser.find_element_by_css_selector('#items > ytd-mini-guide-entry-renderer:nth-child(1)')
